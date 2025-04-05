0e73ee68ff2a5247f17876d557568312102f334f
anasraza07 <ar470684@gmail.com>
2025-04-05 09:59:07 +0500

    todo checked and delete functionality also done

--- app.js
+++ app.js
@@ -4,15 +4,25 @@ const todos = document.querySelector("#todos");
 
 addTodoBtn.addEventListener("click", function () {
     if (todoInput.value) {
-        const newTodo = `
-            <div class="todo flex items-center bg-gray-700 p-3 rounded-lg shadow-md">
-                <input class="w-5 h-5 cursor-pointer" type="checkbox">
-                <span class="flex-1 mx-3">${todoInput.value}</span>
-                <button class="bg-red-600 text-white px-3 py-1 rounded-md cursor-pointer hover:bg-red-500 transition">Delete</button>
-            </div>
+        let newTodo = document.createElement("div");
+        newTodo.className = "todo flex items-center bg-gray-700 p-3 rounded-lg shadow-md";
+        newTodo.innerHTML = `
+            <input class="checkbox-input w-5 h-5 cursor-pointer" type="checkbox">
+            <span class="flex-1 mx-3">${todoInput.value}</span>
+            <button class="delete-btn bg-red-600 text-white px-3 py-1 rounded-md cursor-pointer hover:bg-red-500 transition">Delete</button>
         `;
 
-        todos.innerHTML += newTodo;
+        let checkboxInput = newTodo.querySelector(".checkbox-input");
+        checkboxInput.addEventListener("change", function (event) {
+            checkboxInput.nextElementSibling.classList.toggle("checked");
+        })
+
+        let deleteBtn = newTodo.querySelector(".delete-btn");
+        deleteBtn.addEventListener("click", function () {
+            newTodo.remove();
+        })
+
+        todos.appendChild(newTodo);
         todoInput.value = "";
     }
 })
